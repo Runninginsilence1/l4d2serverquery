0a084fdb7cf8ab4d001561bf5e902c03ccd1628c
Runninginsilence1 <runninginsilence0@gmail.com>
2024-08-29 17:34:38 +0800

    终于弄好gitignore了

---
+++ L4d2ServerQuery.Tests/Service/ServerQueryServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using JetBrains.Annotations;
+using L4d2ServerQuery.Model;
+using L4d2ServerQuery.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace L4d2ServerQuery.Tests.Service;
+
+[TestClass]
+[TestSubject(typeof(ServerQueryService))]
+public class ServerQueryServiceTest
+{
+
+    [TestMethod]
+    public void METHOD()
+    {
+        FavoriteServer favoriteServer = new FavoriteServer()
+        {
+            Host = "42.192.4.35",
+            Port = 42300,
+        };
+        
+        ServerInformation serverInformation = new ServerInformation(favoriteServer);
+
+        for (int i = 0; i < 5; i++)
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine($"Information {serverInformation.GetSteamQueryInformation()?.OnlinePlayers}:");
+        }
+    }
+}
