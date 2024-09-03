1ca3408680ac31c7700aa9eb46a2497c04b81bc7
Runninginsilence1 <runninginsilence0@gmail.com>
2024-09-03 21:08:33 +0800

    提交一个版本

--- L4d2ServerQuery/Program.cs
+++ L4d2ServerQuery/Program.cs
@@ -83,6 +83,8 @@ app.MapPost("/serverAdd", async (FavoriteServer server, FavoriteServerContext db
 
 app.Run();
 
+
+
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
